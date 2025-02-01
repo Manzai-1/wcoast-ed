b24006094aa8a9ea6b9293ea82470171de58ae3b
Manzai-1 <0xdecentmaz@gmail.com>
2025-02-01 15:20:35 +0100

    Improve filter-services.ts by making the functions more readable and better structured

--- dist/utils/filter-services.js
+++ dist/utils/filter-services.js
@@ -2,25 +2,24 @@ import { config } from '../config/config.js';
 import { getData } from './http-services.js';
 export const filterCourses = async (courses, filter) => {
     const filterCourses = [];
-    console.log(filter);
     for (const course of courses) {
         let include = true;
         if (filter.title)
-            include = course.title.toLowerCase().
-                includes(filter.title.toLocaleLowerCase());
+            include =
+                course.title.toLowerCase().includes(filter.title.toLowerCase());
         if (include && !filter.onSite)
             include = course.onSite ? false : true;
         if (include && !filter.remote)
             include = course.remote ? false : true;
         if (include && filter.popular)
-            include = await getCourseUserCount(course.id) >= 3 ? true : false;
+            include = await isPopular(course.id, 3);
         if (include)
             filterCourses.push(course);
     }
     return filterCourses;
 };
-const getCourseUserCount = async (id) => {
+const isPopular = async (id, minUsers) => {
     const registry = await getData(`${config.endpoint.registry}?id=${id}`);
-    const count = registry.length > 0 ? registry[0].users.length : 0;
-    return count;
+    const userCount = registry.length > 0 ? registry[0].users.length : 0;
+    return userCount >= minUsers;
 };
--- src/utils/filter-services.ts
+++ src/utils/filter-services.ts
@@ -4,27 +4,24 @@ import { IFilter } from '../models/IFilter.js';
 import { IRegistration } from '../models/IRegistration';
 import { getData } from './http-services.js';
 
-export const filterCourses = async(
-    courses: ICourse[],
-    filter:IFilter
-    ):Promise<ICourse[]> => {
+export const filterCourses = async(courses: ICourse[],filter:IFilter):Promise<ICourse[]> => {
     const filterCourses: ICourse[] = [];
-    console.log(filter);
     for(const course of courses) {
         let include = true;
 
-        if(filter.title) include = course.title.toLowerCase().
-            includes(filter.title.toLocaleLowerCase());
+        if(filter.title) include = 
+            course.title.toLowerCase().includes(
+            filter.title.toLowerCase());
         if(include && !filter.onSite) include = course.onSite ? false : true;
         if(include && !filter.remote) include = course.remote ? false : true;
-        if(include && filter.popular) include =  await getCourseUserCount(course.id!) >= 3 ? true:false;
+        if(include && filter.popular) include =  await isPopular(course.id!, 3);
         if(include) filterCourses.push(course);
     }
     return filterCourses;
 };
 
-const getCourseUserCount = async(id:string):Promise<number>=>{
+const isPopular = async(id:string, minUsers:number):Promise<boolean>=>{
     const registry:IRegistration[] = await getData(`${config.endpoint.registry}?id=${id}`);
-    const count:number = registry.length > 0 ? registry[0].users.length : 0;
-    return count;
+    const userCount:number = registry.length > 0 ? registry[0].users.length : 0;
+    return userCount >= minUsers;
 }
