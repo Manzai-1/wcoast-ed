27c08a28c9973ee67d39f3cc9b80f7c4166e1bc5
Manzai-1 <0xdecentmaz@gmail.com>
2025-02-03 22:27:28 +0100

    Add additional unit tests to displayMessage function

--- src/unitTests/message.test.ts
+++ src/unitTests/message.test.ts
@@ -18,9 +18,29 @@ describe('Create HTML elements and add to DOM', ()=>{
         document.write(docContent);
     });
 
-    it('Should call displayMessage() to create a popup message, and then find a div with id="login-div"', ()=>{
+    it('Should not find a div in DOM with id="login-div"', ()=>{
+        const elem = document.querySelector('#login-div');
+        expect(elem).toBe(null);
+    });
+
+    it('Should call displayMessage() to create and display a popup message, and then find a div with id="login-div"', ()=>{
         displayMessage('','','#');
-        const elems = document.querySelectorAll('#login-div');
-        expect(elems.length > 0).toBe(true);
+        const elem = document.querySelector('#login-div');
+        expect(elem).not.toBe(null);
+    });
+
+    it('Should call displayMessage() and find elements <h1> & <p> & <a> & <button> , "', ()=>{
+        displayMessage('','','#');
+        const elem = document.querySelector('#login-div');
+
+        const h1 = elem?.querySelector('h1');
+        const p = elem?.querySelector('p');
+        const a = elem?.querySelector('a');
+        const button = elem?.querySelector('button');
+
+        expect(h1).not.toBe(null);
+        expect(p).not.toBe(null);
+        expect(a).not.toBe(null);
+        expect(button).not.toBe(null);
     });
 });
